6b941eefb8a508144ba5f634b3736980f855e192
Ahmed gamal <144253822+Ahmedgamalmohaed@users.noreply.github.com>
2025-01-19 10:46:35 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -3,3 +3,4 @@
 ["document.querySelector", "quiz", "checked", "value"] / quiz.js doesn't contain: ["userAnswer"]
 ["if (userAnswer === correctAnswer)"]
 ["document.querySelector("] / quiz.js doesn't contain: ["Correct! Well done."]
+["That's incorrect. Try again!"]
